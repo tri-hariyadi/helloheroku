1757f480dd1bf817e1a26ec77d3c69714d05fa30
tri-hariyadi <trihariyadi24@gmail.com>
2020-10-27 22:31:32 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello Heroku</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          create by @trihariyadi
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
